e979303008d89c7cfb32a7acc3876616e9813878
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-06-06 18:11:13 -0300

    Start building basic file structure

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = () => {
+  <></>;
+};
+
+export const StyledContainer = styled(Container)`
+  display: flex;
+  background-color: blue;
+  height: 3rem;
+`;
